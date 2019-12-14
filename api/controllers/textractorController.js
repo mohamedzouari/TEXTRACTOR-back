a15eae594274dd390d4c43657c162207f3e26fd7
Mohamed Zouari <zouarimohamed@outlook.com>
2019-12-14 18:38:42 +0100

    Download service completed and tested, export service created

--- api/controllers/textractorController.js
+++ api/controllers/textractorController.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var crypto = require('crypto');
-var fs = require('fs');
+var fs = require('fs-extra');
+var PDFDocument = require('pdfkit');
+var officegen = require('officegen');
 
 var ssn; //Session variable
 
@@ -56,13 +58,44 @@ exports.convert = function(req, res) {
         //verify token
         ssn = req.session.token;
 
+
         if(ssn == req.body.token){
 
-            //converts all images in the folder with the token name
+            //gets all images in the folder with the token name
+            var path = 'api/files/'+ssn+'/';
+            fs.readdir(path, function (err, files) {
+                //handling error
+                if (err) {
+                    return console.log('Unable to scan directory: ' + err);
+                } 
+                //listing all files using forEach
+                files.forEach(function (file) {
+
+                    // file processing
+                    console.log(file); 
+                });
+            });
+
+
+            /* Send files to python API here
+
+                let fetch = require('node-fetch');
+
+                fetch('http://localhost', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: '{}'
+                }).then(response => {
+                return response.json();
+                }).catch(err => {console.log(err);});
+            */
 
             //returns text
 
-        res.json({ token : ssn });
+        res.json({ 
+            token : ssn,
+            text : "converted text"
+        });
 
         }else {
             res.json({ error : 'User not identified' });
@@ -73,25 +106,69 @@ exports.convert = function(req, res) {
 
 exports.download = function(req, res) {
 
-    
-
     //verify token
     ssn = req.session.token;
 
     if(ssn == req.body.token){
 
-        //Modified Text Processing
+        //file format according to choice
+        switch(req.body.fileType){
+            case "pdf":{
+          
+                //Create PDF
+                const doc = new PDFDocument;
+                doc.pipe(fs.createWriteStream('api/files/'+ssn+'/document.pdf'));
+                //customize font
+                doc.font('api/assets/fonts/Roboto-Black.ttf')
+                .fontSize(25)
+                .text('Some text with an embedded font!', 100, 100);
+                doc.end();
+  
+                //Return PDF
+                var file = 'api/files/'+ssn+'/document.pdf' ;
 
-        //Selecting a file format
-        
-        //Returns the file to download
+                res.setHeader('Content-disposition', 'attachment; filename=document.pdf');
+                res.setHeader('Content-type', 'application/pdf');
 
+                var filestream = fs.createReadStream(file);
+                filestream.pipe(res);
+                break;
+            }
+            case "word":{
+
+                // Create the word document
+                let docx = officegen('docx');
+                let pObj = docx.createP();
+                pObj.addText('This is my text',{ bold: true, underline: true,color: '00ffff',align:'center' });    
+
+                docx.on('error', function(err) {
+                    console.log(err);
+                  });
+
+                res.setHeader('Content-disposition', 'attachment; filename=document.docx');
+                res.setHeader('Content-type', 'application/msword');
+
+                docx.generate(res);
+                break;
+            }
+            default:{
+                res.json({text : "text"});
+                break;
+            }
+        }
+
+        //remove temporary folder and close session
+        fs.remove('api/files/'+ssn, (err) => {
+            if(err) {
+                console.log(err);
+            } 
+        }); 
+        //destroy the session
         req.session.destroy((err) => {
             if(err) {
                 return console.log(err);
             }
         });
-        res.json("download");
 
     }else {
         res.json({ error : 'User not identified' });
@@ -104,6 +181,7 @@ exports.donate = function(req, res) {
     //Gets the amount of money to donate
 
     //Donating Process
+        // https://stripe.com/docs/api
 
     //Returns Result
     res.json("donate");
